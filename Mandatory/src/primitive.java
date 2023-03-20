0462927e8729646a82790a0417f10496b2b53852
IlanthendralManimozhi <ilanthendral.manimozhi@cdw.com>
2023-03-20 13:28:14 +0530

    Task-1

---
+++ Mandatory/src/primitive.java
@@ -0,0 +1,24 @@
+
+public class primitive {
+	   static boolean  data1;
+	   static double data2;
+	   static float data3;
+	   static int data4;
+	   static short data5;
+	   static long data6;
+	   static char data7;
+	   static byte data8;
+
+	public static void main(String[] args) {
+		System.out.println(data1);
+		System.out.println(data2);
+		System.out.println(data3);
+		System.out.println(data4);
+		System.out.println(data5);
+		System.out.println(data6);
+		System.out.println(data7);
+		System.out.println(data8);
+		
+	}
+
+}
