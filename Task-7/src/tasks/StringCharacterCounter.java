0a120700e5aa04b155c99d6f14ccc80d119b24c4
IlanthendralManimozhi <ilanthendral.manimozhi@cdw.com>
2023-03-28 20:03:16 +0530

    Task-7 submission

--- Task-7/src/tasks/StringCharacterCounter.java
+++ Task-7/src/tasks/StringCharacterCounter.java
@@ -6,18 +6,10 @@ public class StringCharacterCounter {
 	public static void main(String[] args) {
 		String str; // the string to count words in
 		Scanner sc = new Scanner(System.in);
-		int count = 0; // initialize word count to zero
-        str=sc.nextLine();
-        sc.close();
-		// loop over the string
-		for (int i = 0; i < str.length(); i++) {
-			// check if the current character is a whitespace or the last character in the
-			// string
-			if (Character.isWhitespace(str.charAt(i)) || i == str.length() - 1) {
-				count++; // increment word count
-			}
-		}
+		str = sc.nextLine();
+		sc.close();
+		String[] words = str.split("([\\W\\s]+)");
 
-		System.out.println("Word count: " + count);
+		System.out.println("Word count: " + words.length);
 	}
 }
