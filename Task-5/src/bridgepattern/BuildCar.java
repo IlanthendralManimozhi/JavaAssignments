5aa564073cdb37dd6351bb7d97f4b36b18bfe393
IlanthendralManimozhi <ilanthendral.manimozhi@cdw.com>
2023-03-28 23:17:22 +0530

    Task-5

--- Task-5/src/bridgepattern/BuildCar.java
+++
@@ -1,9 +0,0 @@
-package bridgepattern;
-/**
- * Interface with buildcar abstract method
- * @author ilanthendralmanimozhi
- *
- */
-public interface BuildCar {
-	public void buildcar();
-}
--- Task-5/src/flyweightpattern/Maruti.java
+++
@@ -1,9 +0,0 @@
-package flyweightpattern;
-/**
- *
- * @author ilanthendralmanimozhi
- *
- */
-public interface Maruti {
-	void build();
-}
